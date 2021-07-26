02171e32ecb3ec86c3797bf62fb371ec690dae26
kendrickPC <kennethpchang@gmail.com>
2021-07-26 13:47:01 -0700

    login screen created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import react from 'react';
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
-      React App
+      {/* React App */}
+      <Login />
     </div>
   );
 }
